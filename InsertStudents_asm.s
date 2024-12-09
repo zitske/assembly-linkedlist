67c66e04c0395913dc5104cb16577b3e4dc85439
zitske <eduardomontzitske@gmail.com>
2024-12-09 10:57:33 -0300

    Insert Compilou no Mars

--- InsertStudents_asm.s
+++ InsertStudents_asm.s
@@ -1,79 +1,67 @@
-.text
-.globl InsertStudent_asm
-.extern malloc  # Declarar malloc como função externa
+# Função: InsertStudent
+# Parâmetros:
+#   $a0 = ponteiro para a lista (struct LinkedList *list)
+#   $a1 = ponteiro para o novo estudante (struct Student *newStudent)
 
-InsertStudent_asm:
-    # Salvar o endereço de retorno e o ponteiro de quadro
-    addi $sp, $sp, -8
-    sw $ra, 4($sp)
-    sw $fp, 0($sp)
-    move $fp, $sp
+InsertStudent:
+    # Passo 1: Alocar memória para o novo nó
+    li $v0, 9           # 9 = chamada de sistema sbrk (syscall de malloc)
+    li $a0, 8           # Tamanho da struct Node (2 pointers de 4 bytes)
+    syscall
+    move $t4, $v0       # $t4 armazena o ponteiro para o novo nó
 
-    # Alocar memória para o novo nó
-    li $a0, 8  # sizeof(struct Node)
-    jal malloc
-    move $s0, $v0  # Salvar ponteiro para o novo nó
+    # Passo 2: Configurar o novo nó
+    sw $a1, 0($t4)      # newNode->data = newStudent (armazenar o ponteiro)
+    li $t3, 0           # newNode->next = NULL
+    sw $t3, 4($t4)      # Armazenar NULL no campo 'next' do nó
 
-    # Definir dados do novo nó
-    lw $t0, 12($fp)  # Carregar argumento newStudent
-    sw $t0, 0($s0)  # newNode->data = newStudent
+    # Passo 3: Procurar a posição correta para o novo nó
+    lw $t0, 0($a0)      # Carregar list->head em $t0 (aux = list->head)
+    li $t1, 0           # auxPrev = NULL (início)
+    
+InsertStudent_for:
+    beq $t0, $zero, InsertStudent_end_for # Se aux == NULL, sai do for
 
-    # Inicializar o ponteiro next do novo nó para NULL
-    sw $zero, 4($s0)  # newNode->next = NULL
+    # Carregar registration do estudante no nó atual
+    lw $t2, 0($t0)      # aux->data
+    lw $t5, 0($t2)      # registration = aux->data->registration
+    lw $t6, 0($a1)      # registration de newStudent
+    blt $t6, $t5, InsertStudent_insert_position # Se newStudent->registration < currentRegistration, insere o nó
+    
+    # Atualizar auxPrev e aux
+    move $t1, $t0       # auxPrev = aux
+    lw $t0, 4($t0)      # aux = aux->next
+    j InsertStudent_for # Continua o loop
 
-    # Ponteiros auxiliares
-    lw $t1, 8($fp)  # Carregar argumento list
-    lw $t2, 0($t1)  # aux = list->head
-    move $t3, $zero  # auxPrev = NULL
+InsertStudent_insert_position:
+    # Verifica se o novo nó deve ser o novo head
+    lw $t2, 0($a0)      # list->head
+    beq $t0, $t2, InsertStudent_new_head # Se aux == list->head, insere como novo head
+    
+    # Insere entre dois nós (auxPrev e aux)
+    sw $t0, 4($t4)      # newNode->next = aux
+    sw $t4, 4($t1)      # auxPrev->next = newNode
+    j InsertStudent_update_size # Atualizar o tamanho da lista
 
-    # Procurar o lugar certo para inserir o estudante
-    insert_loop:
-        beq $t2, $zero, insert_end  # Se aux == NULL, sair do loop
-        lw $t4, 0($t2)  # Carregar aux->data
-        lw $t5, 45($t4)  # Carregar currentRegistration
-        lw $t6, 12($fp)  # Carregar newStudent
-        lw $t7, 45($t6)  # Carregar newStudent->registration
-        bge $t7, $t5, next_node  # Se newStudent->registration >= currentRegistration, ir para o próximo nó
+InsertStudent_new_head:
+    sw $t0, 4($t4)      # newNode->next = aux
+    sw $t4, 0($a0)      # list->head = newNode
+    j InsertStudent_update_size # Atualizar o tamanho da lista
 
-        # Inserir antes do primeiro nó (novo nó head)
-        beq $t2, $t1, insert_head
-        # Inserir entre dois nós
-        sw $s0, 4($t3)  # auxPrev->next = newNode
-        sw $t2, 4($s0)  # newNode->next = aux
-        j insert_end
+InsertStudent_end_for:
+    # Caso o for termine, verifica se a lista está vazia ou se o novo nó será o novo tail
+    lw $t7, 8($a0)      # list->size
+    beq $t7, $zero, InsertStudent_empty_list # Se list->size == 0, insere no início
+    
+    # Insere o novo nó no final da lista
+    sw $t4, 4($t1)      # auxPrev->next = newNode
 
-    next_node:
-        move $t3, $t2  # auxPrev = aux
-        lw $t2, 4($t2)  # aux = aux->next
-        j insert_loop
+InsertStudent_empty_list:
+    sw $t4, 0($a0)      # list->head = newNode
+    sw $t4, 4($a0)      # list->tail = newNode
 
-    insert_head:
-        sw $s0, 0($t1)  # list->head = newNode
-        sw $t2, 4($s0)  # newNode->next = aux
-        j insert_end
-
-    insert_end:
-        beq $t2, $zero, insert_tail  # Se aux == NULL, definir nova cauda
-        j update_size
-
-    insert_tail:
-        lw $t8, 4($t1)  # Carregar list->tail
-        beq $t8, $zero, set_head  # Se a lista estiver vazia, definir nova cabeça
-        sw $s0, 4($t8)  # list->tail->next = newNode
-        sw $s0, 4($t1)  # list->tail = newNode
-        j update_size
-
-    set_head:
-        sw $s0, 0($t1)  # list->head = newNode
-        sw $s0, 4($t1)  # list->tail = newNode
-
-    update_size:
-        lw $t9, 8($t1)  # Carregar list->size
-        addi $t9, $t9, 1  # list->size++
-        sw $t9, 8($t1)  # Atualizar tamanho da lista
-
-    # Restaurar ponteiro de quadro e endereço de retorno
-    lw $ra, 4($sp)
-    lw $fp, 0($sp)
-    addi $sp, $sp, 8
-    jr $ra
+InsertStudent_update_size:
+    lw $t7, 8($a0)      # Carregar list->size
+    addi $t7, $t7, 1    # list->size++
+    sw $t7, 8($a0)      # Atualizar list->size
+    jr $ra              # Retorna da função
